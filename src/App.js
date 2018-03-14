38faa4d7adbebc45f43f33bc4e0be7c229a2933f
mickequeen <mickequeen@gmail.com>
2018-03-14 12:37:25 -0300

    body y nav

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav/Nav.js'
+import Content from './components/Content/Content.js'
 
 class App extends Component {
   render() {
