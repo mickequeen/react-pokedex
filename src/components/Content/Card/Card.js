620475ad2e2c0f7e6ebd37502bc11dba8b89f443
mickequeen <mickequeen@gmail.com>
2018-03-15 01:09:22 -0300

    inline elements, name of pokemons

--- src/components/Content/Card/Card.js
+++ src/components/Content/Card/Card.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import pokemonGif from 'pokemon-gif';
+import PropTypes from 'prop-types';
+import pokemonNumberToName from 'pokemon-gif/lib/pokedex-number-to-name';
 import './Card.css';
 
 class Card extends Component {
@@ -6,15 +9,19 @@ class Card extends Component {
     let superReturn = [];
     for (let i = 1; i <= 802; i++) {
       let url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + i + '.png';
-      superReturn.push(this.mostrarPokemon(url, i));
+      const name = pokemonNumberToName[i];
+      superReturn.push(this.mostrarPokemon(url, i, name));
+      console.log(name)
+
     }
     return (superReturn);
-  };
+      };
 
-  mostrarPokemon(url, i) {
+  mostrarPokemon(url, i, name) {
     return (
       <div className='col-md-2 contenedor' onClick={(e) => this.testfunction(i, e)}>
-        <img src={url} id={i}/>;
+        <img src={url} id={i}/>
+        <p>{ name }</p>
       </div>
     );
   };
